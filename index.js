b9b874fca28936134ad4cffc2ab0687202da8a91
Grazielly <gacascardi@outlook.com>
2025-04-07 10:36:49 -0300

    commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("Bem-vindo ao projeto Mimos da Dany!");
 
 // Adicione aqui o código principal do seu projeto
+// Você pode usar funções, classes, módulos, etc. conforme necessário
+// Exemplo de função simples
